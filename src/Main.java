6716b8f05d0340d2f944e91dbca03565b41f93fb
Yevgen <evgeniygal@gmail.com>
2022-10-16 11:42:17 +0300

    Add Shape interface

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import com.drawelement.Shape;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+}
+
+class MyClass implements Shape{
+
+    @Override
+    public double getArea() {
+        double ghh = Shape.PI;
+        return 0;
+    }
+
+    @Override
+    public double getPerimetr() {
+        return 0;
     }
 }
---
+++ src/com/drawelement/Shape.java
@@ -0,0 +1,15 @@
+package com.drawelement;
+
+public interface Shape {
+    double PI = 3.14;
+
+    double getArea();
+
+    double getPerimetr();
+
+    static double getGipotenuza(double k1, double k2) {
+        return Math.sqrt(k1 * k1 + k2 * k2);
+    }
+
+
+}
